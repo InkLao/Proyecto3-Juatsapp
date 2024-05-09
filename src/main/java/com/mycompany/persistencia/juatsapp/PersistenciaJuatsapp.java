e7356288d269dcc785bb3234a60edb5f35a210ea
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-05-09 00:47:40 -0700

    Repositorio inicializado

---
+++ src/main/java/com/mycompany/persistencia/juatsapp/PersistenciaJuatsapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persistencia.juatsapp;
+
+/**
+ *
+ * @author eduar
+ */
+public class PersistenciaJuatsapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
