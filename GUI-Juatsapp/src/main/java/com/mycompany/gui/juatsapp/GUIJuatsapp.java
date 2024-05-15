3ddcfebd738e4ad07cec4d66e4d058e96606a291
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-05-15 15:29:00 -0700

    Diseño Preliminar de los GUI
    Diseños preliminares de la aplicación

---
+++ GUI-Juatsapp/src/main/java/com/mycompany/gui/juatsapp/GUIJuatsapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui.juatsapp;
+
+/**
+ *
+ * @author eduar
+ */
+public class GUIJuatsapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
